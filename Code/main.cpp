a4f0d65ab69ff95746db8c29182887241a4f6c7b
abdullahk150 <abdullahkaleem150@gmail.com>
2025-10-19 15:55:41 +0500

    Password Manager added to manage opeerations( still some debugging required

--- Code/main.cpp
+++ Code/main.cpp
@@ -18,6 +18,49 @@ struct PasswordNode
     }
 };
 
+
+struct PasswordManager
+{
+
+    PasswordNode* head;
+    ActivityQueue activityQueue;    // Queue for activity logging
+
+    PasswordManager() 
+    {
+        head = nullptr;
+    }
+
+    void addPassword() 
+    {
+        string account, pass;
+
+        cin.ignore(); 
+        cout << "\nEnter Account Name (e.g., Gmail, Facebook, Bank): ";
+        getline(cin, account);
+
+        cout << "Enter Password for " << account << ": ";
+        getline(cin, pass);
+
+        checkAndSuggestStrength(pass);
+
+        PasswordNode* newNode = new PasswordNode(account, pass);
+
+        if (!head) 
+        {
+            head = newNode;
+        } 
+        else
+        {
+            PasswordNode* temp = head;
+            while (temp->next) temp = temp->next;
+            temp->next = newNode;
+        }
+
+        cout << "✅ Password for " << account << " added successfully!\n";
+    }
+};
+
+
 int main()
 {
 
